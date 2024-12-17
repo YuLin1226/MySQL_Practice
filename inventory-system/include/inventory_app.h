d4379015fed0b9f54d3ef8a2700abf761badcaac
Ryan Chen <ryanchen.ylc@gmail.com>
2024-12-17 22:21:26 +0800

    update header for app.

--- inventory-system/include/inventory_app.h
+++ inventory-system/include/inventory_app.h
@@ -1,4 +1,8 @@
 #pragma once
+#include "inventory_srv.h"
+#include "warehouse_srv.h"
+#include "product_srv.h"
+
 
 class InventoryApplication
 {
@@ -7,5 +11,11 @@ private:
 public:
     InventoryApplication(/* args */);
     ~InventoryApplication();
+
+    // Result adjustInventory(AdjustmentRequest);
+    // Result queryStock(std::string warehouse_name, std::string product_name);
+    // Result createProduct(ProductRequest);
+    // Result createWarehouse(WarehouseRequest);
+
 };
 
